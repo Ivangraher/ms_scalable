4e092e8d496265e94d818a6a04465d2c3f0a268e
Navy <ivangraherm@gmail.com>
2025-04-08 12:50:40 +0200

    Fix init error eureka server

--- ms-orders/src/main/java/com/example/ms_orders/config/WebClientConfig.java
+++ ms-orders/src/main/java/com/example/ms_orders/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.ms_orders.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Bean
+    /*@Bean
+    @LoadBalanced
     public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8083/api/inventory")
+        //return builder.baseUrl("http://localhost:8084/api/inventory")
+        return builder.baseUrl("lb://ms-inventory/api/inventory")
                 .build();
+    }*/
+
+
+    @Bean
+    @LoadBalanced
+    public WebClient client(){
+        return WebClient.builder().baseUrl("lb://ms-inventory/api/inventory").build();
     }
 }
 
